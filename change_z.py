9b4f14b6c566dbf7226fd9a5b9e950584ce618d5
NikitaMulyar <nickm.school@mail.ru>
2023-01-28 13:12:34 +0300

    function @z changing@

--- change_z.py
+++ change_z.py
@@ -1,13 +1,11 @@
 import pygame
 
 
-def change_z():
+def change_z(event, z):
     pygame.init()
-    z = 0
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                z += 1
-            elif event.key == pygame.K_DOWN:
-                z -= 1
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+            z += 1
+        elif event.key == pygame.K_DOWN:
+            z -= 1
     return z
