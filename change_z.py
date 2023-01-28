4dd3d62c30bc18d7188b1d73c4efe877af83f127
NikitaMulyar <ni***ol@mail.ru>
2023-01-28 23:37:59 +0300

    - Пятая часть задачи (без 4 части)

--- change_z.py
+++ change_z.py
@@ -8,19 +8,19 @@ def change_z(event, z, x, y):
     y = float(y)
     CONST_X = 4.56555 * 2 ** (z - 2.63)
     CONST_Y = 8 * 2 ** (z - 2.46)
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_PAGEUP:
-            if z < 16:
-                z += 1
-        elif event.key == pygame.K_PAGEDOWN:
-            if z > 2:
-                z -= 1
-        if event.key == pygame.K_UP:
-            y += 450 / CONST_Y
-        elif event.key == pygame.K_RIGHT:
-            x += 600 / CONST_X
-        elif event.key == pygame.K_DOWN:
-            y -= 450 / CONST_Y
-        elif event.key == pygame.K_LEFT:
-            x -= 600 / CONST_X
+    if event.key == pygame.K_PAGEUP:
+        if z < 16:
+            z += 1
+    elif event.key == pygame.K_PAGEDOWN:
+        if z > 2:
+            z -= 1
+    if event.key == pygame.K_UP:
+        y += 450 / CONST_Y
+    elif event.key == pygame.K_RIGHT:
+        x += 600 / CONST_X
+    elif event.key == pygame.K_DOWN:
+        y -= 450 / CONST_Y
+    elif event.key == pygame.K_LEFT:
+        x -= 600 / CONST_X
+    print(1)
     return z, x, y
--- main.py
+++ main.py
@@ -2,10 +2,48 @@ import os
 import pygame
 import requests
 from change_z import change_z
+from consts import *
 
 
-def request(size, coord1, coord2):
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord1},{coord2}&l=map&z={size}"
+class TextInputBox(pygame.sprite.Sprite):
+    def __init__(self, x, y, clr, bg, w, h, text=''):
+        super().__init__()
+        self.color = clr
+        self.bg = bg
+        self.pos = (x, y)
+        self.font = pygame.font.Font(None, 40)
+        self.text = text
+        self.rect_ = pygame.rect.Rect(x, y, w, h)
+
+    def render(self, screen):
+        screen.blit(self.font.render(self.text, True, self.color, self.bg), self.pos)
+        pygame.draw.rect(screen, self.color, self.rect_.inflate(5, 5), 2)
+
+
+def get_coors(name):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": name,
+        "format": "json"}
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_api_server)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        return [0, 0]
+
+    json_response = response.json()
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    toponym_coodrinates = toponym["Point"]["pos"]
+    toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+    return [toponym_longitude, toponym_lattitude]
+
+
+def request(size, coord):
+    map_request = f"http://static-maps.yandex.ru/1.x/?ll={coord[0]},{coord[1]}&l=map&z={size}"
     response = requests.get(map_request)
 
     if not response:
@@ -19,28 +57,50 @@ def request(size, coord1, coord2):
         file.write(response.content)
 
 
-def main(coord, size):
-    request(size, *coord)
+def main():
+    size = 10
+    coord = get_coors('Москва')
+    request(size, coord)
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
     screen.blit(pygame.image.load("map.png"), (0, 0))
     pygame.display.flip()
     run = True
     data = [size, coord[0], coord[1]]
+    text_box = TextInputBox(10, 400, (255, 255, 255), (200, 50, 100), 350, 40)
+    check_box = TextInputBox(400, 400, (255, 255, 255), (200, 200, 100), 150, 40, text='Искать!')
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
-            data = list(change_z(event, *data))
-        request(*data)
+            if event.type == pygame.KEYDOWN:
+                """if event.key == pygame.K_RETURN:
+                    if text_box.text != '':
+                        request(data[0], text_box.text)"""
+                if event.key == pygame.K_BACKSPACE:
+                    text_box.text = text_box.text[:-1]
+                else:
+                    if event.unicode in TO_RUS:
+                        text_box.text += TO_RUS[event.unicode]
+                    elif event.unicode in TO_RUS_BIG:
+                        text_box.text += TO_RUS_BIG[event.unicode]
+                    else:
+                        text_box.text += event.unicode
+                data = list(change_z(event, *data))
+                request(data[0], data[1:])
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if check_box.rect_.colliderect(pygame.rect.Rect(*pygame.mouse.get_pos(), 5, 5)):
+                    if text_box.text != '':
+                        data = [data[0], *get_coors(text_box.text)]
+                        request(data[0], data[1:])
         screen.blit(pygame.image.load("map.png"), (0, 0))
+        text_box.render(screen)
+        check_box.render(screen)
         pygame.display.flip()
     pygame.quit()
     os.remove("map.png")
 
 
 if __name__ == '__main__':
-    coord = input('Введите координаты через пробел (долгота, широта): ').split()
-    size = input('Введите масштаб карты: ')
-    main(coord, size)
+    main()
