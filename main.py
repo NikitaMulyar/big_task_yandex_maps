d044dd5dbae1c064debf66c81f5c677b070a8282
NikitaMulyar <ni***ol@mail.ru>
2023-02-03 21:53:59 +0300

    - Задачи 8,9,10 без 4,6 и 7 частей

--- main.py
+++ main.py
@@ -6,18 +6,42 @@ from consts import *
 
 
 class TextInputBox(pygame.sprite.Sprite):
-    def __init__(self, x, y, clr, bg, w, h, text=''):
+    def __init__(self, x, y, clr, bg, w, h, size, text=''):
         super().__init__()
         self.color = clr
         self.bg = bg
         self.pos = (x, y)
-        self.font = pygame.font.Font(None, 40)
+        self.font = pygame.font.Font(None, size)
         self.text = text
         self.rect_ = pygame.rect.Rect(x, y, w, h)
 
     def render(self, screen):
         screen.blit(self.font.render(self.text, True, self.color, self.bg), self.pos)
         pygame.draw.rect(screen, self.color, self.rect_.inflate(5, 5), 2)
+
+
+class IndexBox(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.color = (255, 255, 255)
+        self.bg = (200, 0, 0)
+        self.pos = (x, y)
+        self.font = pygame.font.Font(None, 30)
+        self.text = 'ind'
+        self.rect_ = pygame.rect.Rect(x, y, 30, 30)
+
+    def on_off(self):
+        return self.bg == (0, 200, 0)
+
+    def change_clr(self):
+        if self.bg == (200, 0, 0):
+            self.bg = (0, 200, 0)
+        else:
+            self.bg = (200, 0, 0)
+
+    def render(self, screen):
+        pygame.draw.rect(screen, self.bg, self.rect_)
+        screen.blit(self.font.render(self.text, True, self.color), self.pos)
 
 
 def get_coors(name):
@@ -32,14 +56,20 @@ def get_coors(name):
         print("Ошибка выполнения запроса:")
         print(geocoder_api_server)
         print("Http статус:", response.status_code, "(", response.reason, ")")
-        return [0, 0]
+        return [0, 0], ''
 
     json_response = response.json()
     toponym = json_response["response"]["GeoObjectCollection"][
         "featureMember"][0]["GeoObject"]
+    adrs = toponym['metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['AddressLine']
+    try:
+        index = toponym['metaDataProperty']['GeocoderMetaData']['Address']['postal_code']
+        print('rrrrrr')
+    except Exception:
+        index = ''
     toponym_coodrinates = toponym["Point"]["pos"]
     toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
-    return [toponym_longitude, toponym_lattitude]
+    return [toponym_longitude, toponym_lattitude], adrs, index
 
 
 def request(size, coord):
@@ -59,7 +89,7 @@ def request(size, coord):
 
 def main():
     size = 10
-    coord = get_coors('Москва')
+    coord = get_coors('Москва')[0]
     request(size, coord)
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
@@ -67,8 +97,10 @@ def main():
     pygame.display.flip()
     run = True
     data = [size, coord[0], coord[1]]
-    text_box = TextInputBox(10, 400, (255, 255, 255), (200, 50, 100), 350, 40)
-    check_box = TextInputBox(400, 400, (255, 255, 255), (200, 200, 100), 150, 40, text='Искать!')
+    text_box = TextInputBox(10, 400, (255, 255, 255), (200, 50, 100), 350, 40, 40)
+    check_box = TextInputBox(400, 400, (255, 255, 255), (200, 200, 100), 150, 40, 40, text='Искать!')
+    address_box = TextInputBox(10, 10, (255, 255, 255), (0, 50, 100), 540, 20, 20)
+    index_box = IndexBox(560, 10)
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -90,13 +122,27 @@ def main():
                 data = list(change_z(event, *data))
                 request(data[0], data[1:])
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if check_box.rect_.colliderect(pygame.rect.Rect(*pygame.mouse.get_pos(), 5, 5)):
+                if index_box.rect_.collidepoint(*pygame.mouse.get_pos()):
+                    index_box.change_clr()
+                    res = get_coors(text_box.text)
+                    if index_box.on_off():
+                        address_box.text = res[2] + ' ' + res[1]
+                    else:
+                        address_box.text = res[1]
+                if check_box.rect_.collidepoint(*pygame.mouse.get_pos()):
                     if text_box.text != '':
-                        data = [data[0], *get_coors(text_box.text)]
+                        res = get_coors(text_box.text)
+                        data = [data[0], *res[0]]
                         request(data[0], data[1:])
+                        if index_box.on_off():
+                            address_box.text = res[2] + ' ' + res[1]
+                        else:
+                            address_box.text = res[1]
         screen.blit(pygame.image.load("map.png"), (0, 0))
         text_box.render(screen)
         check_box.render(screen)
+        address_box.render(screen)
+        index_box.render(screen)
         pygame.display.flip()
     pygame.quit()
     os.remove("map.png")
